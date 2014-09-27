16c47f9b4945b5ed9a7c69d69df6017ce29ef550
finley hebert-perkins <finley.hebert@gmail.com>
2014-09-27 14:20:58 -0700

    added adduser.py

---
+++ adduser.py
@@ -0,0 +1,7 @@
+username = raw_input("username: ")
+password = raw_input("password: ")
+email = raw_input("email: ")
+from tinydb import TinyDB
+db = TinyDB("dbs/user.json")
+db.insert({"username" : username, "password" : password, "email" : email, "permission" : 0})
+print "added", username
