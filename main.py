22b54111fbdd29b66ac3ce9c157b056987925934
finley hebert-perkins <finley.hebert@gmail.com>
2014-11-11 23:03:18 -0800

    Nov-11-1103PM-2014

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from app import momentjs, check_hash, get_theme, parse_post
 from hashlib import md5
 from datetime import datetime
+from uuid import uuid4
 
 
 
@@ -16,11 +17,11 @@ Users = MongoClient().helpdesk.profiles.Users
 Tickets = MongoClient().helpdesk.data.Tickets
 
 def get_unread():
-	if Tickets.find({"read" : False}) == None:
+	if Tickets.find({"status" : 0}) == None:
 		return "0"
 	else:
-		tickets = Tickets.find({"read" : False})
-		return str(tickets.explain()["nscanned"])
+		tickets = Tickets.find({"status" : 0})
+		return str(tickets.count())
 
 @app.before_request
 def setup():
@@ -36,21 +37,40 @@ def index():
 		return redirect(url_for("login", redirect=request.url))
 	page = int(request.args.get("p", "0"))
 	tickets = []
-	[tickets.append(ticket) for ticket in Tickets.find()]
+	[tickets.append(ticket) for ticket in Tickets.find().sort("time", -1)]
 	app.jinja_env.globals['title'] = "View Tickets"
 	tickets = tickets[(page*10):(page*10)+10]
 	return render_template("tickets_list.html", tickets=tickets)
 
-@app.route("/details/<url>")
+@app.route("/details/<url>", methods=["post", "get"])
 def details(url):
 	if not session.get("logged_in"):
 		return redirect(url_for("login", redirect=request.url))
 	ticket = Tickets.find_one({"url" : url})
-	app.jinja_env.globals['title'] = ticket["title"]
 	if ticket == None:
 		return 404
+	app.jinja_env.globals['title'] = ticket["title"]
+	if request.method == "POST":
+		try:
+			replies = ticket["reply"]
+		except:
+			replies = []
+		reply_dict ={
+					"author" : session.get("username"),
+					"time" : datetime.utcnow(),
+					"content" : parse_post(request.form["text"])
+					}
+		replies.append(reply_dict)
+		Tickets.update({"_id" : ticket["_id"]}, {"$set" : {"reply" : replies}})
+		flash("reply submitted")
+		return render_template("ticket_detail.html", ticket=ticket)
+	if ticket["status"] == 0:
+		Tickets.update({"_id" : ticket["_id"]}, {"$set" : {"status" : 1}})
+	new_status = request.args.get("u", None)
+	if new_status != None and new_status != 0 and new_status != 1 and new_status <= 4:
+		Tickets.update({"_id" : ticket["_id"]}, {"$set" : {"status" : new_status}})
 	else:
-		return render_template("detail.html", ticket=ticket)
+		return render_template("ticket_detail.html", ticket=ticket)
 
 @app.route("/new", methods=['GET', 'POST'])
 def new_thread():
@@ -63,18 +83,37 @@ def new_thread():
 			if request.form["is_vip"]:
 				is_vip = True
 		except:
-			flash("vip")
+			pass
 		tid = Tickets.insert({"title": request.form["title"],
-							"url" : md5(request.form["title"]).hexdigest(),
+							"url" : uuid4().hex,
 							"content" : parse_post(request.form["text"]),
 							"time" : datetime.utcnow(),
 							"status" : 0,
 							"author" : request.form["author"],
 							"is_vip" : is_vip,
-							"read" : False,
+							"reply" : [],
 							"importance" : int(request.form["urgency"])})
-		return str(tid)
+		flash("created new ticket <a href='/detail/{0}'>here</a> ".format(md5(request.form["title"]).hexdigest()))
 	return render_template("new_ticket.html")
+
+@app.route("/user/<user>")
+def user_page(user):
+	if not session.get("logged_in"):
+		return redirect(url_for("login", redirect=request.url))
+	user_data = Users.find_one({"username" : user})
+	if user_data == None:
+		return 404
+	app.jinja_env.globals["title"] = user
+	recent_tickets = Tickets.find({"author" : user}).sort("time", -1)[0:10]
+	tickets_submitted = {
+						"total_submitted" : Tickets.find({"author" : user}).count(),
+						"percentage" : (float(Tickets.find({"author" : user}).count())/float(Tickets.find().count()))*100
+						}
+	return render_template("user_profile.html", user=user_data, tickets=tickets_submitted, recent_tickets= recent_tickets)
+
+@app.route("/settings")
+def settings():
+	return "NYI"
 
 @app.route("/login", methods=["post", "get"])
 def login():
@@ -83,10 +122,13 @@ def login():
 			return redirect(url_for("index"))
 		return redirect(request.args.get("redirect", None))
 	if request.method == "POST":
-			password = Users.find_one({"username" : request.form["username"]})["password"]
+			user = Users.find_one({"username" : request.form["username"]})
+			password = user["password"]
 			if check_hash(request.form["password"], password[0], password[1]):
 				session["logged_in"] = True
 				session["username"] = request.form["username"]
+				session["theme"] = user.get("theme", None)
+				Users.update({"_id" : user["_id"]}, {"$set" : {"last_login" : datetime.utcnow()}})
 				if request.args.get("redirect", None) == None:
 					return redirect(url_for("index"))
 				return redirect(request.args.get("redirect", None))
@@ -102,5 +144,5 @@ def logout():
 
 if __name__ == "__main__":
 	import os
-	app.secret_key=os.urandom(16)
+	app.secret_key=os.urandom(24)
 	app.run(debug=True)
